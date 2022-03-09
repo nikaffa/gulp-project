d04afe832124dfeb5f7af71778d7e5f0030f8bd3
Veronika Fatikhova <fidelio@list.ru>
2022-03-09 15:01:54 -0800

    add eslint, ejs, browsersync modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,54 @@
 const { src, dest, watch } = require("gulp");
 const sass = require('gulp-sass')(require('sass'));
+const sync = require("browser-sync").create();
+const ejs = require("gulp-ejs");
+const rename = require("gulp-rename");
+const eslint = require("gulp-eslint");
 
 const generateCSS = (cb) => {
   src('styles/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(dest('public/stylesheets'));
+    .pipe(dest('public/stylesheets'))
+    .pipe(sync.stream());
   cb();
 };
-
 exports.css = generateCSS;
 
-const watchFiles = (cb) => {
-  watch('styles/*.scss', generateCSS);
-};
 
-exports.watch = watchFiles;
+const generateHTML = (cb) => {
+  src("views/index.ejs")
+    .pipe(ejs({
+      title: "Hello Cyper",
+    }))
+    .pipe(rename({
+      extname: ".html"
+    }))
+    .pipe(dest("public"));
+  cb();
+};
+exports.html = generateHTML;
+
+
+const runLinter = (cb) => {
+  return src(['**/*.js', '!node_modules/**'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+    .on('end', function() {
+      cb();
+    });
+};
+exports.lint = runLinter;
+
+
+const browserSync = (cb) => {
+  sync.init({
+    server: {
+      baseDir: "./public"
+    }
+  });
+  watch('views/**.ejs', generateHTML);
+  watch('styles/*.scss', generateCSS);
+  watch("public/**.html").on('change', sync.reload);
+};
+exports.sync = browserSync;
