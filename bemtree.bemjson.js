c6c5f189e1767ed24bcc03d36046835f2fb36feb
Veronika Fatikhova <fidelio@list.ru>
2022-03-16 14:03:56 -0700

    cleanup html, add bemjson

--- bemtree.bemjson.js
+++ bemtree.bemjson.js
@@ -1,13 +1,75 @@
-// block: 'body', 'body_view-lead'{
+/* eslint-disable  */
 
-//   header: 'header_view-lead'[
-//     { elem: 'meta', attrs: { name: 'description', content: '' }},
-//     { elem: 'css', url: 'index.min.css' }
-// ],
+//BEM tree manual
+body
+  header
+    header__logo
+    nav__menu
+      nav__item
+        nav__img
+        nav__lnk
+      nav__line
+    header__avatar
+    header__line
 
-//     footer: 'footer_view-lead' [
-//         { elem: 'meta', attrs: { name: 'description', content: '' }},
-//         { elem: 'css', url: 'index.min.css' }
-//     ],
+  filters
+    filters__section
+      form
+        form__label
+        form__select
+          form__select_text
+          form__select_option
+      form
+        form__input_checkbox
+        form__label
+    btn_apply
+    btn_reset
+
+  card__proj-info
+    card__decor
+    card__name
+    card__content
+      table__info
+        tr
+          td
   
-// }
+  card__proj-time
+    card__decor
+    card__name
+    card__content_multiple
+      card__time_total
+        table__time
+          tr
+            td
+      card__time_cur
+        table__cur
+          tr
+            td
+      card__time_estim
+        table__estim
+          tr
+            td
+  
+  card__eff
+    card__decor
+    card__name
+    card__content-double
+      card__time_estim
+        table__estim
+          tr
+            td
+   
+  card__single
+    card__row
+      table__snglproj
+        tr
+          td
+    card__content-nomargin
+      card__row
+        table__snglproj
+          tr
+            td
+  
+  footer
+
+
