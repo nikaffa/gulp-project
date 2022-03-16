31938a30037163e713f984c81d1665194d2c3d5b
Veronika Fatikhova <fidelio@list.ru>
2022-03-16 09:54:53 -0700

    bug fix in ejs

--- bemtree.bemjson.js
+++ bemtree.bemjson.js
@@ -1,13 +1,13 @@
-block: 'body', 'body_view-lead'{
+// block: 'body', 'body_view-lead'{
 
-  header: 'header_view-lead'[
-    { elem: 'meta', attrs: { name: 'description', content: '' }},
-    { elem: 'css', url: 'index.min.css' }
-],
+//   header: 'header_view-lead'[
+//     { elem: 'meta', attrs: { name: 'description', content: '' }},
+//     { elem: 'css', url: 'index.min.css' }
+// ],
 
-    footer: 'footer_view-lead' [
-        { elem: 'meta', attrs: { name: 'description', content: '' }},
-        { elem: 'css', url: 'index.min.css' }
-    ],
+//     footer: 'footer_view-lead' [
+//         { elem: 'meta', attrs: { name: 'description', content: '' }},
+//         { elem: 'css', url: 'index.min.css' }
+//     ],
   
-}
+// }
